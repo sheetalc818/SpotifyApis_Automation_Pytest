2064e608c0f377c24a30af00fb9e838077f72a8c
sheetalc818 <sheetalc818@gmail.com>
2020-07-07 15:27:43 +0530

    ADDED : Script for Post Api

--- Spotify/test_spotify_api.py
+++ Spotify/test_spotify_api.py
@@ -1,35 +1,52 @@
 import json
+
+import pytest
 import requests
+
 
 # pytest
 
 class TestClass:
 
+    @pytest.fixture
+    def spotify_token(self):
+        token = 'Bearer BQBa9rV6VmY1U9VNBLKsU3Ljy9ukQLjlTVUQiKB1zyNEAtVGOeUo5zZFJkoNo6Ctm2ng8NVgoy2nOR3GZF3w4eObXjHjpuYrPfqO-TDSsKWK9D1JU9fWf_CghXWXT7lHEQBFDhxrkpo0xvSoBNEY2s1Oy7N1LA69yT00YXREiWPiOTfD5mmEe4Er_QGNV5TqBM3HRBo3_vmBFQ0CsKntba6Lj_hQu-NvpL2SzH8clvj1zSViQwrUE-GfkDZkhlu9K9wrGcLLPJI9jbgFg3F-jxqgOe7EQTUW'
+        headers = {
+            'authorization': token,
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/json'
+        }
+        return headers
+
     def test_given_valid_url_check_status_code_equals_200(self):
         response = requests.get("https://api.spotify.com")
         assert response.status_code == 200
 
-    def test_get_current_user_check_status_code_equals_200(self):
+    def test_get_current_user_check_status_code_equals_200(self, spotify_token):
         url = 'https://api.spotify.com/v1/me'
-        user_ID = 'miq9x438ysuedi1io43rz0awv'
-        headers = {
-            'authorization': 'Bearer BQCNRDdTq1EgEJkH9oIdcInM1po64Ytw0QfCVu3FyiHRqBun-_qJlSxmlM_Cnw7K_bMzJL5XNEo1MoI6vqnUx9XjmWCisnY5KwlI0PgcvIP61OSJgXw41vhiTjGUYhQISRxkdjKbYxW003yj0Flb2ZwjrIH1U_qmlnJs3lGzJd89DzAH_E0si7rkTn8VasGbvDGU2RBiL7VJdBH6wCBQcc9aStxudCKGdiZygd6hXWOs7O8P4w30takWDww5ITGt-X3o5yArTS82iJU-_WrCZH53Wz_6kQVF',
-            'cache-control': 'no-cache'
-        }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=spotify_token)
         assert response.status_code == 200
+        response_body = response.json()
+        global user_id
+        user_id = response_body['id']
+        assert user_id == 'miq9x438ysuedi1io43rz0awv'
 
-        json_response = response.json()
-        print(json_response)
+    def test_creating_playlist_check_status_code_equals_200(self, spotify_token):
+        global user_id
+        total=5
+        url = 'https://api.spotify.com/v1/users/' + user_id + '/playlists'
+        payload = {'name': 'BTS Songs', 'description': 'Playlist for BTS Songs', 'public': True}
+        response = requests.post(url, headers=spotify_token, data=json.dumps(payload))
+        createResponseBody = response.json()
+        assert createResponseBody['name'] == 'BTS Songs' and response.status_code == 201
+        print(json.dumps(createResponseBody))
 
-        data = json.load(response.content, 'id')
-        assert data['id'] == user_ID
+    def test_get_current_user_playlist_check_status_code_equals_200(self, spotify_token):
+        global user_id
+        url = 'https://api.spotify.com/v1/users/' + user_id + '/playlists'
 
-    def test_get_current_user_playlist_check_status_code_equals_200(self):
-        url = 'https://api.spotify.com/v1/me/playlist'
-        headers = {
-            'authorization': 'Bearer BQCNRDdTq1EgEJkH9oIdcInM1po64Ytw0QfCVu3FyiHRqBun-_qJlSxmlM_Cnw7K_bMzJL5XNEo1MoI6vqnUx9XjmWCisnY5KwlI0PgcvIP61OSJgXw41vhiTjGUYhQISRxkdjKbYxW003yj0Flb2ZwjrIH1U_qmlnJs3lGzJd89DzAH_E0si7rkTn8VasGbvDGU2RBiL7VJdBH6wCBQcc9aStxudCKGdiZygd6hXWOs7O8P4w30takWDww5ITGt-X3o5yArTS82iJU-_WrCZH53Wz_6kQVF',
-            'cache-control': 'no-cache'
-        }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=spotify_token)
         assert response.status_code == 200
+        getPlaylist_Response = response.json()
+        total_playlist = getPlaylist_Response['total']
+        assert total_playlist == 9
